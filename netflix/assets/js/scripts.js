6e3f049320803672b5805d196a75bca7881b4e6b
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2022-01-20 23:51:39 -0300

    .

--- netflix/assets/js/scripts.js
+++ netflix/assets/js/scripts.js
@@ -12,18 +12,3 @@ const changeBarColor = () => {
 }
 
 window.addEventListener("scroll", changeBarColor)
-
-// Javascript vanilla
-// var nav = document.querySelector('.navbar');
-// window.onscroll = function (evt) {
-
-//     if (window.scrollY < 10 && !onTop) {
-//         nav.classList.add('onTop');
-//         onTop = true;
-//     }
-
-//     if (window.scrollY > 10 && onTop) {
-//         nav.classList.remove('onTop');
-//         onTop = false;
-//     }
-// };
